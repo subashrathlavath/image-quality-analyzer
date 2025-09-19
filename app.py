7567ff596b310fecab7603f00aebb1c9940f527c
subashrathlavath <subashrathlavath@outlook.com>
2025-09-19 09:29:52 +0000

    commit

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"]
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess
     img_tensor = transform(image).unsqueeze(0)
